c7f93c91395684f28c7243b0e2b03cf8e6d8364f
Carlos Longarela <carlos@longarela.eu>
2019-11-12 17:28:21 +0100

    Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-var AppName = 'cl-woo-dorset-waivers';
-
 var gulp         = require( 'gulp' ),
 	ts           = require( 'gulp-typescript' ),
 	uglify       = require( 'gulp-uglify' ),
@@ -14,20 +12,12 @@ var gulp         = require( 'gulp' ),
 
 var tsconfig = require( './tsconfig.json' );
 
-var AdminTS = [
-	'./admin/js/src/' + AppName + '-admin.ts',
+var GeneralTS = [
+	'./__src/typescript/*.ts',
 ];
 
-var PublicTS = [
-	'./public/js/src/' + AppName + '-public.ts',
-];
-
-var AdminSCSS = [
-	'./admin/css/scss/' + AppName + '-admin.scss',
-];
-
-var PublicSCSS = [
-	'./public/css/scss/' + AppName + '-public.scss',
+var GeneralSCSS = [
+	'./__src/scss/style.scss',
 ];
 
 var plugins = [
@@ -36,11 +26,11 @@ var plugins = [
 ];
 
 /** Js Tasks */
-gulp.task( 'admin-scripts', function() {
-	var js_dest      = 'admin/js';
-	var js_post_name = '-admin.min.js';
+gulp.task( 'general-scripts', function() {
+	var js_dest      = 'js/';
+	var js_post_name = '.min.js';
 
-	return gulp.src( AdminTS )
+	return gulp.src( GeneralTS )
 		.pipe( ts( tsconfig.compilerOptions ) )
 		.pipe( sourcemaps.init() )
 		.pipe( sourcemaps.identityMap() )
@@ -51,37 +41,17 @@ gulp.task( 'admin-scripts', function() {
 		.pipe( gulp.dest( js_dest ) )
 		.pipe( notify( {
 			title: 'Gulp ADMIN JS Task Result:',
-			message: 'Admin [/' + js_dest + '/' + AppName + js_post_name + '] created.',
-			onLast: true
-		} ) );
-} );
-
-gulp.task( 'public-scripts', function() {
-	var js_dest      = 'public/js';
-	var js_post_name = '-public.min.js';
-
-	return gulp.src( PublicTS )
-		.pipe( ts( tsconfig.compilerOptions ) )
-		.pipe( sourcemaps.init() )
-		.pipe( sourcemaps.identityMap() )
-		.pipe( plumber() )
-		.pipe( concat( AppName + js_post_name ) )
-		.pipe( uglify() )
-		.pipe( sourcemaps.write( './maps' ) )
-		.pipe( gulp.dest( js_dest ) )
-		.pipe( notify( {
-			title: 'Gulp PUBLIC JS Task Result:',
-			message: 'Public [/' + js_dest + '/' + AppName + js_post_name + '] created.',
+			message: 'Admin [/' + js_dest + '/' + js_post_name + '] created.',
 			onLast: true
 		} ) );
 } );
 
 /** SCSS Tasks */
-gulp.task( 'admin-scss', function() {
-	var css_dest      = 'admin/css';
-	var css_post_name = '-admin.min.css';
+gulp.task( 'general-scss', function() {
+	var css_dest      = 'css';
+	var css_post_name = '.min.css';
 
-	return gulp.src( AdminSCSS )
+	return gulp.src( GeneralSCSS )
 		.pipe( sourcemaps.init() )
 		.pipe( sourcemaps.identityMap() )
 		.pipe( plumber() )
@@ -92,39 +62,17 @@ gulp.task( 'admin-scss', function() {
 		.pipe( gulp.dest( css_dest ) )
 		.pipe( notify( {
 			title: 'Gulp ADMIN CSS Task Result:',
-			message: 'Admin [/' + css_dest + '/' + AppName + css_post_name + '] created.',
-			onLast: true
-		} ) );
-} );
-
-gulp.task( 'public-scss', function() {
-	var css_dest      = 'public/css';
-	var css_post_name = '-public.min.css';
-
-	return gulp.src( PublicSCSS )
-		.pipe( sourcemaps.init() )
-		.pipe( sourcemaps.identityMap() )
-		.pipe( plumber() )
-		.pipe( concat( AppName + css_post_name ) )
-		.pipe( sass().on( 'error', sass.logError ) )
-		.pipe( postcss( plugins ) )
-		.pipe( sourcemaps.write( './maps' ) )
-		.pipe( gulp.dest( css_dest ) )
-		.pipe( notify( {
-			title: 'Gulp PUBLIC CSS Task Result:',
-			message: 'Public [/' + css_dest + '/' + AppName + css_post_name + '] created.',
+			message: 'Admin [/' + css_dest + '/' + css_post_name + '] created.',
 			onLast: true
 		} ) );
 } );
 
 gulp.task( 'watch', function() {
 	// Inspect changes in ts files.
-	gulp.watch( AdminTS, gulp.series( 'admin-scripts' ) );
-	gulp.watch( PublicTS, gulp.series( 'public-scripts' ) );
+	gulp.watch( AdminTS, gulp.series( 'general-scripts' ) );
 
 	// Inspect changes in scss files.
-	gulp.watch( AdminSCSS, gulp.series( 'admin-scss' ) );
-	gulp.watch( PublicSCSS, gulp.series( 'public-scss' ) );
+	gulp.watch( AdminSCSS, gulp.series( 'general-scss' ) );
 } );
 
 gulp.task( 'default', gulp.parallel( 'watch' ) );
