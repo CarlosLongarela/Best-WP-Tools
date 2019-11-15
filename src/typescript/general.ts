9d108280ca1958bbb287e5f909b539c17d233627
Carlos Longarela <carlos@longarela.eu>
2019-11-15 22:59:23 +0100

    analytics

--- src/typescript/general.ts
+++ src/typescript/general.ts
@@ -1,22 +1,4 @@
-( function () {
-	'use strict';
 
-	// Google Analytics.
-	var _gaq = Array()
-	_gaq = _gaq || [];
-	_gaq.push( ['_setAccount', 'UA-74049753-5'] );
-	_gaq.push( ['_trackPageview'] );
-
-	( function() {
-		var ga   = document.createElement( 'script' );
-		ga.type  = 'text/javascript';
-		ga.async = true;
-		ga.src   = 'https://ssl.google-analytics.com/ga.js';
-		var s    = document.getElementsByTagName( 'script' )[0];
-		s.parentNode.insertBefore( ga, s );
-	} )();
-
-}() );
 
 /* eslint-disable no-unused-vars */
 var utils = {
