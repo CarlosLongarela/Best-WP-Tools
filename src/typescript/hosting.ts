33690cfbf2fd6f30377217bcbe57ca01fc129901
Carlos Longarela <carlos@longarela.eu>
2023-04-05 02:05:22 +0200

    Deleted all non-used pages

--- src/typescript/hosting.ts
+++
@@ -1,6 +0,0 @@
-document.addEventListener( 'DOMContentLoaded', () => {
-	utils.translator( document.body ); // Only if html page has body strings to translate.
-	//utils.translator( document.head ); // Only if html page has head strings to translate.
-
-	utils.show_data_div(); // Show principal content.
-} );
